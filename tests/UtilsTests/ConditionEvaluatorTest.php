380b4af596693b44f16276fcee5438b6c522d563
aliabbasrizvi <ali@optimizely.com>
2016-12-12 12:04:23 +0530

    Adding test with Not condition check

--- tests/UtilsTests/ConditionEvaluatorTest.php
+++ tests/UtilsTests/ConditionEvaluatorTest.php
@@ -2,14 +2,14 @@
 /**
  * Copyright 2016, Optimizely
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the \"License\");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an \"AS IS\" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -29,7 +29,7 @@ class ConditionEvaluatorTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $decoder = new ConditionDecoder();
-        $conditions = "[\"and\", [\"or\", [\"or\", {\"name\": \"device_type\", \"type\": \"custom_attribute\", \"value\": \"iPhone\"}]], [\"or\", [\"or\", {\"name\": \"location\", \"type\": \"custom_attribute\", \"value\": \"San Francisco\"}]]]";
+        $conditions = "[\"and\", [\"or\", [\"or\", {\"name\": \"device_type\", \"type\": \"custom_attribute\", \"value\": \"iPhone\"}]], [\"or\", [\"or\", {\"name\": \"location\", \"type\": \"custom_attribute\", \"value\": \"San Francisco\"}]], [\"or\", [\"not\", [\"or\", {\"name\": \"browser\", \"type\": \"custom_attribute\", \"value\": \"Firefox\"}]]]]";
         $decoder->deserializeAudienceConditions($conditions);
 
         $this->conditionsList = $decoder->getConditionsList();
@@ -41,7 +41,7 @@ class ConditionEvaluatorTest extends \PHPUnit_Framework_TestCase
         $userAttributes = [
             'device_type' => 'iPhone',
             'location' => 'San Francisco',
-            'browser' => 'chrome'
+            'browser' => 'Chrome'
         ];
 
         $this->assertTrue($this->conditionEvaluator->evaluate($this->conditionsList, $userAttributes));
@@ -51,8 +51,9 @@ class ConditionEvaluatorTest extends \PHPUnit_Framework_TestCase
     public function testEvaluateConditionsDoNotMatch()
     {
         $userAttributes = [
+            'device_type' => 'iPhone',
             'location' => 'San Francisco',
-            'browser' => 'chrome'
+            'browser' => 'Firefox'
         ];
 
         $this->assertFalse($this->conditionEvaluator->evaluate($this->conditionsList, $userAttributes));
