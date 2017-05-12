32d1b646b2b76ca158eaeca43fde608d6dac176b
Mike Ng <mike.ng@optimizely.com>
2017-05-12 14:50:14 -0700

    Introduce decision service.
    Summary: Refactor decision-making to the decision service.
    
    Test Plan: Unit and e2e.
    
    Reviewers: #oasis_team_review
    
    JIRA Issues: OASIS-1145
    
    Differential Revision: https://phabricator.optimizely.com/D15942

--- tests/BucketerTest.php
+++ tests/BucketerTest.php
@@ -281,30 +281,4 @@ class BucketerTest extends \PHPUnit_Framework_TestCase
             $bucketer->bucket($this->config, new Experiment(), $this->testUserId)
         );
     }
-
-    public function testGetForcedVariationExperimentNotInGroupUserInForcedVariation()
-    {
-        $bucketer = new Bucketer($this->loggerMock);
-        $this->loggerMock->expects($this->once())
-            ->method('log')
-            ->with(Logger::INFO, 'User "user1" is forced in variation "control" of experiment "test_experiment".');
-
-        $this->assertEquals(
-            new Variation('7722370027', 'control'),
-            $bucketer->getForcedVariation($this->config, $this->config->getExperimentFromKey('test_experiment'), 'user1')
-        );
-    }
-
-    public function testGetForcedVariationExperimentInGroupUserInForcedVariation()
-    {
-        $bucketer = new Bucketer($this->loggerMock);
-        $this->loggerMock->expects($this->once())
-            ->method('log')
-            ->with(Logger::INFO, 'User "user1" is forced in variation "group_exp_1_var_1" of experiment "group_experiment_1".');
-
-        $this->assertEquals(
-            new Variation('7722260071', 'group_exp_1_var_1'),
-            $bucketer->getForcedVariation($this->config, $this->config->getExperimentFromKey('group_experiment_1'), 'user1')
-        );
-    }
 }
