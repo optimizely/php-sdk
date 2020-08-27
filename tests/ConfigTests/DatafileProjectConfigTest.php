764e40ba84ba68a34d2c177d434824f3d4613e68
uzair-folio3 <unadeem@folio3.com>
2020-08-27 11:11:49 +0500

    Update DatafileProjectConfigTest.php

--- tests/ConfigTests/DatafileProjectConfigTest.php
+++ tests/ConfigTests/DatafileProjectConfigTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2020, Optimizely
+ * Copyright 2016-2020, Optimizely
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
