7adcf214ee3b9d4e5d0504a2a9060a89b53bfaa8
mnoman09 <m.nomanshoaib09@gmail.com>
2021-12-15 19:53:37 +0500

    added unit test and comments

--- src/Optimizely/Event/Builder/EventBuilder.php
+++ src/Optimizely/Event/Builder/EventBuilder.php
@@ -251,8 +251,11 @@ class EventBuilder
     {
         $eventParams = $this->getCommonParams($config, $userId, $attributes);
         $experiment = $config->getExperimentFromId($experimentId);
+
+        // Mapped flagKey can be directly used in variation in that case no experimentKey exist
         $variation = $config->getFlagVariationByKey($flagKey, $variationKey);
 
+        // When variation is not mapped to any flagKey 
         if (!$variation) {
             $variation = $config->getVariationFromKeyByExperimentId($experimentId, $variationKey);
         }
