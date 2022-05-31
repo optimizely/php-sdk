6692337d81908e6ae706c4f41ba64f35dd927a27
Muhammad Noman <Muhammadnoman@folio3.com>
2022-05-31 22:54:19 +0500

    chore: prepare for release 3.9.1 (#248)
    * chore: prepare for release 3.9.1
    
    * Update CHANGELOG.md
    
    * added 249 pr as part of this changelog.
    
    Co-authored-by: Mirza Sohail Hussain <mirza.sohailhussain@gmail.com>

--- src/Optimizely/Event/Builder/EventBuilder.php
+++ src/Optimizely/Event/Builder/EventBuilder.php
@@ -39,7 +39,7 @@ class EventBuilder
     /**
      * @const string Version of the Optimizely PHP SDK.
      */
-    const SDK_VERSION = '3.9.0';
+    const SDK_VERSION = '3.9.1';
 
     /**
      * @var string URL to send event to.
