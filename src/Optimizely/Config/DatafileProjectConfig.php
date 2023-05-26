9b2612301c2685fc862019da391ca1dfc9b1a9d5
Mike Chu <michael.chu@optimizely.com>
2023-05-26 09:00:37 -0400

    Add missing Copyright years

--- src/Optimizely/Config/DatafileProjectConfig.php
+++ src/Optimizely/Config/DatafileProjectConfig.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2019-2021, Optimizely
+ * Copyright 2019-2021, 2023 Optimizely
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
