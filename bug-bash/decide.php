e7c18c7eaa235f6566dd3c05ac6e6bbef62b5adf
Mike Chu <michael.chu@optimizely.com>
2023-05-31 13:50:10 -0400

    Output log to ensure logx impression sent

--- bug-bash/decide.php
+++ bug-bash/decide.php
@@ -5,7 +5,9 @@ namespace Optimizely\BugBash;
 require_once '../vendor/autoload.php';
 require_once '../bug-bash/bug-bash-autoload.php';
 
+use Monolog\Logger;
 use Optimizely\Decide\OptimizelyDecideOption;
+use Optimizely\Logger\DefaultLogger;
 use Optimizely\Notification\NotificationType;
 use Optimizely\Optimizely;
 use Optimizely\OptimizelyFactory;
@@ -41,7 +43,7 @@ class DecideTests
         $this->printDecision($decision, 'Check that the following decision properties are expected');
     }
 
-    //   test decide w all options: DISABLE_DECISION_EVENT, ENABLED_FLAGS_ONLY, IGNORE_USER_PROFILE_SERVICE, INCLUDE_REASONS, EXCLUDE_VARIABLES (will need to add variables)
+    // test decide w all options: DISABLE_DECISION_EVENT, ENABLED_FLAGS_ONLY, IGNORE_USER_PROFILE_SERVICE, INCLUDE_REASONS, EXCLUDE_VARIABLES (will need to add variables)
     public function testWithVariationsOfDecideOptions(): void
     {
         $options = [
@@ -57,9 +59,31 @@ class DecideTests
         $this->printDecision($decision, 'Modify the OptimizelyDecideOptions and check the decision variables expected');
     }
 
-    //   verify in logs that impression event of this decision was dispatched
+    // verify in logs that impression event of this decision was dispatched
     public function verifyLogsImpressionsEventsDispatched(): void
     {
+        // Create a new flag with an A/B Test eg "product_version"
+        $featureFlagKey = 'product_version';
+        $logger = new DefaultLogger(Logger::DEBUG);
+        $localOptimizelyClient = new Optimizely(datafile: null, logger: $logger, sdkKey: SDK_KEY);
+        $userId = 'user-' . mt_rand(10, 99);
+        $localUserContext = $localOptimizelyClient->createUserContext($userId);
+
+        // review the DEBUG output, ensuring you see an impression log
+        // "Dispatching impression event to URL https://logx.optimizely.com/v1/events with params..."
+        $localUserContext->decide($featureFlagKey);
+    }
+
+    // verify on Results page that impression even was created
+    public function verifyResultsPageInYourProjectShowsImpressionEvent(): void
+    {
+        print "Go to your project's results page and verify decisions events are showing (might be a delay)";
+    }
+
+    // verify that decision listener contains correct information
+    public function verifyDecisionListenerWasCalled(): void
+    {
+        // Check that this was called during the...
         $onDecision = function ($type, $userId, $attributes, $decisionInfo) {
             print ">>> [NotificationCenter] OnDecision:
             type: $type,
@@ -67,27 +91,16 @@ class DecideTests
             attributes: " . print_r($attributes, true) . "
             decisionInfo: " . print_r($decisionInfo, true) . "\r\n";
         };
-        
         $this->optimizelyClient->notificationCenter->addNotificationListener(
             NotificationType::DECISION,
             $onDecision
         );
 
+        // ...decide.
         $this->userContext->decide(FLAG_KEY);
     }
 
-    //   verify on Results page that impression even was created
-    public function verifyResultsPageInYourProjectShowsImpressionEvent(): void
-    {
-        print 'Go to your project\'s results page and verify the decision events';
-    }
-
-    //   verify that decision listener contains correct information
-    public function verifyDecisionListenerWasCalled(): void
-    {
-    }
-
-    //   verify that invalid flag key is handled correctly
+    // verify that invalid flag key is handled correctly
     public function verifyAnInvalidFlagKeyIsHandledCorrectly(): void
     {
     }
