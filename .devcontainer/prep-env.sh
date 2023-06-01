a15ce1989358aa3baed6f656938bc840e0b79408
Mike Chu <michael.chu@optimizely.com>
2023-06-01 15:48:31 +0000

    Finish devcontainer config

--- .devcontainer/prep-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# install php dependencies
-composer install
-
-# turn off xdebug messages
-echo "xdebug.remote_enable = 0" | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini > /dev/null
