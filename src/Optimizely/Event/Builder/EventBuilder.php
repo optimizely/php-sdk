e197aa4ecc2691c72ca99df01a7fe7d9b993661e
Matjaz Pirnovar <Mat001@users.noreply.github.com>
2023-06-12 11:53:49 -0700

    [FSSDK-9415] Prep for 4.0.0 release (#272)
    * Update EventBuilder.php w version 4.0.0
    
    * Update EventBuilderTest.php
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    * Update README.md

--- src/Optimizely/Event/Builder/EventBuilder.php
+++ src/Optimizely/Event/Builder/EventBuilder.php
@@ -39,7 +39,7 @@ class EventBuilder
     /**
      * @const string Version of the Optimizely PHP SDK.
      */
-    const SDK_VERSION = '3.9.4';
+    const SDK_VERSION = '4.0.0';
 
     /**
      * @var string URL to send event to.
